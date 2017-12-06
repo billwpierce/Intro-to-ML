17ec59f859e0455504252086472a31ef5772596c
billwpierce <wwpierce020602@gmail.com>
2017-12-06 09:33:44 -0800

    changed discount rate

--- final/final_si.py
+++ final/final_si.py
@@ -22,7 +22,7 @@ class DQNAgent:
         self.input_shape = state_shape
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
+        self.gamma = 0.99    # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.999
