c360d4efcf3e8d333cf7b24a6288410ebc10dcba
billwpierce <wwpierce020602@gmail.com>
2017-12-11 10:51:11 -0800

    changing hyper parameters

--- final/customDQN.py
+++ final/customDQN.py
@@ -28,7 +28,7 @@ class QNet:
 		self.memory = deque(maxlen=2000)
 		self.discount_rate = 0.99
 		self.exploration = 1.0
-		self.exploration_decay = 0.999
+		self.exploration_decay = 0.995
 		self.exploration_min = 0.1
 		self.learning_rate = 0.5
 		self.model = self._build_model()
@@ -43,7 +43,7 @@ class QNet:
 		model.add(Dense(512, activation='relu'))
 		model.add(Dense(self.action_size, activation='linear'))
 		model.compile(loss=categorical_crossentropy,
-			optimizer=SGD(lr=0.1),
+			optimizer=SGD(lr=0.08),
 			metrics=['accuracy'])
 		return model
 
